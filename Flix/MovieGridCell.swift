f7d4811da0db999d990a517d1c0de99d2cc21764
Daniel Cruz-Castro <Dancruzcast@gmail.com>
2021-09-18 02:53:04 -0500

    Completed Unit 2: added grid layout along with tab navigator, bug with safe view appears to have technically resolved itself with the integration of the tab bar controller, unsure if this is good practice though. Autolayout so far has been integrated where proper/applicable however instructions were unclear on how implementation of autolayout could resolve image sizing issues with grid cells. Still needs debugging, implementation of grid cell autolayout, and maybe investigation into details view safe mode.

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,12 @@
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Daniel Cruz Castro on 9/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
